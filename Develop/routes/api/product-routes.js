5af67a0f2b9cd8c664e6c1e43cb5a43d7b443567
Jacob <jacobgasper@gmail.com>
2021-10-26 16:46:43 -0400

    completed remaining routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -124,6 +124,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.Destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'Product ID not found' });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
